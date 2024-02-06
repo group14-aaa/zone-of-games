cb049db22b9e267f0e367d3d93bfded39e08efea
pmAdriaan <pmAdriaan@users.noreply.github.com>
2024-02-06 21:28:33 +0000

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',
+  build: {
+    outDir: 'build/',
+    chunkSizeWarningLimit: 1000,
+    target: 'esnext',
+    polyfill: false, // disable polyfill
+  },
+  server: {
+    middleware: [
+      (req, res, next) => {
+        // Set cache control headers for static assets
+        if (req.url.includes('/assets/')) {
+          res.setHeader('Cache-Control', 'public, max-age=31536000'); // 1 year in seconds
+        }
+
+        // Continue to the next middleware
+        next();
+      },
+    ],
+  },
+});
