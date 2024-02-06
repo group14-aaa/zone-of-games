e449068edfe9d9bc312f0f8823db548066fe2ece
pmAdriaan <pmAdriaan@users.noreply.github.com>
2024-02-06 22:19:52 +0000

    Project clean up and home page add

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Hello gamers! </h1>
+            <p>Made with ❤️ by Mihai, Chris, Anjal and Adam.</p>
+        </>
+    );
+}
+
+export default Home;
