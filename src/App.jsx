e8c090d3fb20fa10259158300f0ee19c5073de19
Anjal Sali <anjalsali55@gmail.com>
2024-02-08 17:04:56 +0000

    defined grid for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Components
-import PageLayout from './components/PageLayout';
-import { getGamesByGenre } from './services/rawgApi';
-import TopRatedGames from './components/GetTopRatedGames';
-
+import PageLayout from "./components/PageLayout";
+import { getGamesByGenre } from "./services/rawgApi";
+import TopRatedGames from "./components/GetTopRatedGames";
 
 // Pages
-import Home from './pages/Home';
-import { ThemeContext } from './context/ThemeContext';
+import Home from "./pages/Home";
+import { ThemeContext } from "./context/ThemeContext";
 
 function App() {
-  const [theme, setTheme] = useState('light');
-  const [games, setGames] = useState([]);
+   const [theme, setTheme] = useState("light");
+   const [games, setGames] = useState([]);
 
-  // Update document root class when theme changes
-  useEffect(() => {
-    document.documentElement.className = theme;
-  }, [theme]);
+   // Update document root class when theme changes
+   useEffect(() => {
+      document.documentElement.className = theme;
+   }, [theme]);
 
-  useEffect(() => {
-    getGamesByGenre('action')
-      .then((gamesData) => {
-        setGames(gamesData);
-      })
-      .catch((error) => {
-        console.error('Error fetching games:', error);
-      });
-  }, []);
+   useEffect(() => {
+      getGamesByGenre("action")
+         .then((gamesData) => {
+            setGames(gamesData);
+         })
+         .catch((error) => {
+            console.error("Error fetching games:", error);
+         });
+   }, []);
 
    // Log games data only once when the component mounts
-   console.log('Games in App component:', games);
+   console.log("Games in App component:", games);
 
-
-
-  return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className={`${theme} ${theme === 'dark' ? 'bg-background' : 'bg-background'} h-[100vh]`}>
-        <PageLayout>
-          <Home />
-        </PageLayout>
-    <TopRatedGames />
-      </div>
-    </ThemeContext.Provider>
-  );
+   return (
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+         <div className={`${theme} ${theme === "dark" ? "bg-background" : "bg-background"} h-[100vh]`}>
+            <PageLayout>
+               <Home />
+            </PageLayout>
+         </div>
+      </ThemeContext.Provider>
+   );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-const Home = () => {
-    return (
-        <>
-            <h1 className="text-text">Hello gamers! </h1>
-        </>
-    );
+import React from "react";
+import TopRatedGames from "../components/GetTopRatedGames";
+function Home() {
+   return (
+      <div className="grid grid-cols-4">
+         <div className="hidden md:block">Sidebar</div>
+         <div className="col-span-4 md:col-span-3">
+            <TopRatedGames />
+         </div>
+      </div>
+   );
 }
 
 export default Home;
