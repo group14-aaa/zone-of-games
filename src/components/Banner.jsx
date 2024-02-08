728d2a770a749a1bb59b2deaaf79ddfe01b1e1a2
Anjal Sali <anjalsali55@gmail.com>
2024-02-08 21:06:11 +0000

    added basic carousal without styling

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import rawgApi from "../services/rawgApi";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-function Banner(gameBanner) {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "../style.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+function Banner() {
+   const [gameBannerList, setGameBannerList] = useState([]);
+
    useEffect(() => {
-      console.log(gameBanner);
+      // Fetch top games list when component mounts
+      getGameBannerList();
    }, []);
-   return <div></div>;
+   const getGameBannerList = async () => {
+      try {
+         const response = await rawgApi.getGamesList;
+         // display data to the console
+         // console.log(response.data.results);
+
+         // Sort the games by rating in descending order
+         const gamesList = response.data.results;
+
+         const games = gamesList.slice(0, 9);
+
+         setGameBannerList(games);
+      } catch (error) {
+         console.error("Error fetching top rated games:", error);
+      }
+   };
+   return (
+      <>
+         <Swiper cssMode={true} navigation={true} pagination={true} mousewheel={true} keyboard={true} modules={[Navigation, Pagination, Mousewheel, Keyboard]} className="mySwiper">
+            {gameBannerList.map((game) => (
+               <SwiperSlide key={game.id}>
+                  <img src={game.background_image} alt={game.name} style={{ width: "100%" }} />
+                  <p style={{ textAlign: "center" }}>{game.name}</p>
+               </SwiperSlide>
+            ))}
+         </Swiper>
+      </>
+   );
 }
 
 export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import Sidebar from "../components/Sidebar";
 import RawgTopRatedGames from "../components/RawgTopRatedGames";
 import TwitchTopGames from "../components/TwitchTopGames";
+import Banner from "../components/Banner";
 // import TwitchTopStreams from '../components/TwitchTopStreams';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Sidebar />
          </div>
          <div className="col-span-4 md:col-span-3 bg-primary text-text">
+            <Banner />
             <RawgTopRatedGames />
             <TwitchTopGames />
             {/* <TwitchTopStreams /> */}
