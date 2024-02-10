97d6a8b661f7acd9cda89305fa86f34488ba8dd0
Anjal Sali <anjalsali55@gmail.com>
2024-02-10 03:44:08 +0000

    added nav items to header along with configuration for custome colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
 // Components
-import { ThemeContext } from '../context/ThemeContext';
+import { ThemeContext } from "../context/ThemeContext";
 
 // Images
-import logo from './../assets/images/logo-no-background.png'
+import logo from "./../assets/images/logo-no-background.png";
 
 // Icons
 import { IoSearchSharp } from "react-icons/io5";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 
-
 const Header = () => {
-    const { theme, setTheme } = useContext(ThemeContext);
+   const { theme, setTheme } = useContext(ThemeContext);
 
-    return (
-        <div className='flex items-center p-3 border-b-2 border-accent'>
-            <img src={logo} width={120} height={60} alt='Logo image' />
-            <div className='flex bg-slate-200 p-2 w-full mx-5 rounded-full items-center'>
-                <IoSearchSharp />
-                <input type='text' placeholder='Search Games...' className='px-2 bg-transparent outline-none w-full' />
+   return (
+      <div className="flex items-center p-3 border-b-2 border-accent">
+         <img src={logo} width={100} height={50} alt="Logo image" />
+         <div className="flex bg-slate-200 p-2 w-full mx-5 rounded-full items-center">
+            <IoSearchSharp />
+            <input type="text" placeholder="Search Games..." className="px-2 bg-transparent outline-none w-full" />
+         </div>
+         <div className="hidden sm:mx-6 sm:block">
+            <div className="flex space-x-4">
+               <a href="#" className="text-white hover:bg-navItemColor rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
+                  Home
+               </a>
+               <a href="#" className="text-white hover:bg-navItemColor hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                  About
+               </a>
+               <a href="#" className="text-white hover:bg-navItemColor hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                  Contact
+               </a>
             </div>
-            <div>
-                {theme === 'light' ?
-                    <MdDarkMode
-                        className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer'
-                        onClick={() => { setTheme('dark'); localStorage.setItem('theme', 'dark') }}
-                    /> :
-                    <MdLightMode
-                        className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer'
-                        onClick={() => { setTheme('light'); localStorage.setItem('theme', 'light') }}
-                    />}
-            </div>
-        </div>
-    );
-}
+         </div>
+         <div>
+            {theme === "light" ? (
+               <MdDarkMode
+                  className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer"
+                  onClick={() => {
+                     setTheme("dark");
+                     localStorage.setItem("theme", "dark");
+                  }}
+               />
+            ) : (
+               <MdLightMode
+                  className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer"
+                  onClick={() => {
+                     setTheme("light");
+                     localStorage.setItem("theme", "light");
+                  }}
+               />
+            )}
+         </div>
+      </div>
+   );
+};
 
 export default Header;
