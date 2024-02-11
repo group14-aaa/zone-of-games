3808f4252f7e51c75f269fcd551a2676bef2a8b8
Anjal Sali <anjalsali55@gmail.com>
2024-02-11 19:59:33 +0000

    created base contact page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import PageLayout from "./components/PageLayout";
 // Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Contact from './pages/contact';
+import Contact from "./pages/Contact";
 import Streams from "./pages/streams/";
 import GamePage from "./pages/games/GamePage";
 import ViewStreams from "./pages/streams/ViewStreams";
@@ -20,7 +20,7 @@ import ErrorPage from "./pages/404";
 const routes = [
    { path: "/", component: Home },
    { path: "/about", component: About },
-   // { path: '/Contact', component: Contact },
+   { path: "/Contact", component: Contact },
    { path: "/streams/", component: Streams },
    { path: "/games/:gId", component: GamePage },
    { path: "/streams/:gameId", component: ViewStreams },
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+function Contact() {
+   return (
+      <>
+         <div>Contacts</div>
+      </>
+   );
+}
+
+export default Contact;
