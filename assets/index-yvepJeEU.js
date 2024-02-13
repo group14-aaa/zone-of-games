b2c67305b805cfea643cc71efa6bd0034a54953b
pmAdriaan <pmadriaan@gmail.com>
2024-02-13 21:34:35 +0000

    Build: (f0988b4) Merge pull request #35 from group14-aaa/adam/addingPlatform
    fixed bug where genres not showing when platform has been selected

---
+++ assets/index-yvepJeEU.js
@@ -0,0 +1,7 @@
+import{r,_ as t,j as s}from"./index-keGz_NnW.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-vGrq1OHR.js"),__vite__mapDeps([0,1,2,3,4]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-vGrq1OHR.js","assets/index-keGz_NnW.js","assets/index-BgXAfCmq.css","assets/twitchApi-wtBVE4M-.js","assets/axios-L6U4YIEh.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
