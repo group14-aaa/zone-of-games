f5af27fc28ec3811a9ff1a71c3d123b41085c06d
Anjal Sali <anjalsali55@gmail.com>
2024-02-13 05:51:57 +0000

    added descripton for our team section along with form style change

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -70,6 +70,10 @@ function Contact() {
          <div className="mx-auto px-4 py-8 text-center">
             <div className="text-center">
                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-text">Our Team</h2>
+               <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+                  At Zone of Games, we believe the heart of our success lies within the collective spirit, talent, and dedication of our team. Our diverse group of professionals brings together a rich
+                  tapestry of expertise, backgrounds, and perspectives, united by a common passion for excellence.
+               </p>
             </div>
             <div className="flex flex-wrap justify-center grid  md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
                {profileLinks.map((profile, index) => (
@@ -97,7 +101,7 @@ function Contact() {
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-text">Contact Us</h2>
                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-                  Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
+                  Got a technical issue? Want to send feedback about a beta feature? Need details about our Project? Let us know.
                </p>
                <form onSubmit={handleSubmit} className="space-y-8">
                   <div>
@@ -110,7 +114,7 @@ function Contact() {
                         name="email"
                         value={formData.email}
                         onChange={handleChange}
-                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                        className="shadow-sm bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                         placeholder="Enter your Email address"
                         required
                      />
@@ -125,7 +129,7 @@ function Contact() {
                         name="subject"
                         value={formData.subject}
                         onChange={handleChange}
-                        className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                        className="block p-3 w-full text-sm text-gray-900 bg-gray-300 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                         placeholder="Let us know how we can help you"
                         required
                      />
@@ -139,7 +143,7 @@ function Contact() {
                         value={formData.message}
                         onChange={handleChange}
                         rows="6"
-                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-300 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                         placeholder="Leave a comment..."
                         required
                      ></textarea>
