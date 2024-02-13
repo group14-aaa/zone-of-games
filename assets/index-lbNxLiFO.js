6bea52ef3b376f841d13fa55e4458f464ac18db8
anjalsali <anjalsali55@gmail.com>
2024-02-13 20:39:32 +0000

    Build: (2672a66) Merge branch 'main' of https://github.com/group14-aaa/zone-of-games

--- assets/index-lbNxLiFO.js
+++
@@ -1,7 +0,0 @@
-import{r as e,_ as i,j as t}from"./index-8nRFiHle.js";import{r as c}from"./rawgApi-JX0N44-y.js";import"./axios-L6U4YIEh.js";const m=e.lazy(()=>i(()=>import("./RawgTopRatedGames-k5K12YJ8.js"),__vite__mapDeps([0,1,2]))),d=()=>{const[a,r]=e.useState([]);e.useEffect(()=>{o()},[]);const o=async()=>{try{const s=await c.getGamesList;r(s.data.results)}catch(s){console.error("Error fetching top rated games:",s)}};return t.jsx("div",{className:"grid grid-cols-4",children:t.jsx("div",{className:"col-span-4 bg-primary text-text",children:t.jsx(m,{gamesList:a})})})};export{d as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/RawgTopRatedGames-k5K12YJ8.js","assets/index-8nRFiHle.js","assets/index-BgXAfCmq.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
---
+++ assets/index-zYaVbQjc.js
@@ -0,0 +1,7 @@
+import{r as e,_ as i,j as t}from"./index-afvJwPSE.js";import{r as c}from"./rawgApi-JX0N44-y.js";import"./axios-L6U4YIEh.js";const m=e.lazy(()=>i(()=>import("./RawgTopRatedGames-i7clHQ3G.js"),__vite__mapDeps([0,1,2]))),d=()=>{const[a,r]=e.useState([]);e.useEffect(()=>{o()},[]);const o=async()=>{try{const s=await c.getGamesList;r(s.data.results)}catch(s){console.error("Error fetching top rated games:",s)}};return t.jsx("div",{className:"grid grid-cols-4",children:t.jsx("div",{className:"col-span-4 bg-primary text-text",children:t.jsx(m,{gamesList:a})})})};export{d as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/RawgTopRatedGames-i7clHQ3G.js","assets/index-afvJwPSE.js","assets/index-BgXAfCmq.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
