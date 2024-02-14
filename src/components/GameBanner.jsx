b824be97377b13d4e9797a2dfea27ba41de38b5f
Anjal Sali <anjalsali55@gmail.com>
2024-02-14 11:26:53 +0000

    added basic 3d effect to the slides

--- src/components/GameBanner.jsx
+++ src/components/GameBanner.jsx
@@ -7,57 +7,47 @@ import "swiper/css/navigation";
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import { Link } from "react-router-dom";
 
-const GameBanner = ({ gameList }) => {
-   const carouselSettings = {
-      slidesPerView: 3, // Adjust based on desired width or number of items
-      centeredSlides: true,
-      loop: true,
-      effect: "coverflow",
-      coverflowEffect: {
-         rotate: 50, // Adjust for desired 3D effect
-         stretch: 0, // Control card stretching (0-100)
-         depth: 100, // Set z-axis depth (higher = stronger effect)
-         modifier: 2, // Adjust how slides slide past each other (1-5)
-      },
-      pagination: {
-         el: ".swiper-pagination",
-         clickable: true,
-      },
-      navigation: {
-         nextEl: ".swiper-button-next",
-         prevEl: ".swiper-button-prev",
-      },
-      breakpoints: {
-         // Optional breakpoints for responsive adjustments
-         640: {
-            slidesPerView: 1,
-         },
-         768: {
-            slidesPerView: 2,
-         },
-         1024: {
-            slidesPerView: 3,
-         },
-      },
-   };
+import "../assets/styles/swiperstyle.css";
 
+const GameBanner = ({ gameList }) => {
    return (
-      <div className="swiper-container">
-         <h1 className="heading">Top 10 Games</h1>
-         <Swiper {...carouselSettings}>
-            {gameList.slice(0, 10).map((game, index) => (
-               <SwiperSlide key={game.id}>
-                  <Link to={`/game/${game.id}`}>
-                     <img src={game.background_image} alt={`Game ${index + 1}`} style={{ width: "100%", height: "auto" }} />
-                     <div className="game-info">
-                        <h4>{game.name}</h4>
-                        <span>{game.genres.map((genre) => genre.name).join(", ")}</span>
-                     </div>
-                  </Link>
-               </SwiperSlide>
-            ))}
-         </Swiper>
-      </div>
+      <Swiper
+         effect={"coverflow"}
+         grabCursor={true}
+         centeredSlides={true}
+         slidesPerView={"auto"}
+         coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+         }}
+         pagination={true}
+         modules={[EffectCoverflow, Pagination]}
+         className="mySwiper"
+      >
+         {gameList.slice(0, 10).map((game, index) => (
+            <SwiperSlide key={game.id}>
+               <Link to={`/game/${game.id}`}>
+                  <img src={game.background_image} className="w-100 h-50 object-cover" alt={`Game ${index + 1}`} />
+                  <div className="game-info">
+                     <h4>{game.name}</h4>
+                     <span>{game.genres.map((genre) => genre.name).join(", ")}</span>
+                  </div>
+               </Link>
+            </SwiperSlide>
+         ))}
+         <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow">
+               <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div className="swiper-button-next slider-arrow">
+               <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
+         </div>
+      </Swiper>
    );
 };
 
