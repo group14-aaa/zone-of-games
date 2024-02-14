a0834179c85c043bf16cd39e16b2dc0487e00f60
anjalsali <anjalsali55@gmail.com>
2024-02-14 20:42:42 +0000

    Build: (df84401) Merge pull request #39 from group14-aaa/anjal/gameSearch
    Anjal/game search

---
+++ assets/index-_ZvL_rn2.js
@@ -0,0 +1,7 @@
+import{r,_ as t,j as s}from"./index-8uqjUf3R.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-DvH1djnP.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-DvH1djnP.js","assets/index-8uqjUf3R.js","assets/index-PSgxgiHY.css","assets/twitchApi-nj9rluPa.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
