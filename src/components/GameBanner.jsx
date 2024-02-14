861f8a2a841549df1d0ba0dcf4c2d856be5998e1
Anjal Sali <anjalsali55@gmail.com>
2024-02-14 12:13:31 +0000

    made the slider as a cube

--- src/components/GameBanner.jsx
+++ src/components/GameBanner.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
+import "swiper/css/effect-cube";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { EffectCoverflow, Pagination, Navigation, Autoplay } from "swiper/modules";
+import { EffectCube, Pagination, Navigation, Autoplay } from "swiper/modules";
 import { Link } from "react-router-dom";
 
 import "../assets/styles/swiperstyle.css";
@@ -12,30 +13,27 @@ import "../assets/styles/swiperstyle.css";
 const GameBanner = ({ randomGames }) => {
    return (
       <Swiper
-         effect={"coverflow"}
+         effect={"cube"}
          grabCursor={true}
-         centeredSlides={true}
-         slidesPerView={"auto"}
-         coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
+         cubeEffect={{
+            shadow: true,
             slideShadows: true,
+            shadowOffset: 25,
+            shadowScale: 0.94,
          }}
          autoplay={{
-            delay: 2000,
+            delay: 3000,
             disableOnInteraction: false,
          }}
          pagination={{
             clickable: true,
          }}
-         modules={[EffectCoverflow, Pagination, Autoplay, Navigation]}
+         modules={[EffectCube, Pagination, Autoplay, Navigation]}
          className="mySwiper"
       >
          {randomGames.map((game, index) => (
             <SwiperSlide key={game.id}>
-               <Link to={`/game/${game.id}`}>
+               <Link to={`/games/${game.id}`}>
                   <img src={game.background_image} alt={`Game ${index + 1}`} />
                   <div className="game-info">
                      <h4 className="text-center">{game.name}</h4>
