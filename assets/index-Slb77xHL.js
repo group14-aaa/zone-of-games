1e36e02c14c68d781caf131ab8728ef6a5f49795
anjalsali <anjalsali55@gmail.com>
2024-02-15 07:35:20 +0000

    Build: (a1227f0) Merge pull request #43 from group14-aaa/anjal/styling
    Anjal/styling

---
+++ assets/index-Slb77xHL.js
@@ -0,0 +1,7 @@
+import{r,_ as t,j as s}from"./index-qZllD5Q1.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-CpxKn03V.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-CpxKn03V.js","assets/index-qZllD5Q1.js","assets/index-RwHdFi54.css","assets/twitchApi-4sdPw-jE.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
