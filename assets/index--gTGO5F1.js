367b14fcad5a60207ad5b82e6ca9da70f0b76f22
anjalsali <anjalsali55@gmail.com>
2024-02-15 09:16:41 +0000

    Build: (0b2cccb) Merge branch 'main' of https://github.com/group14-aaa/zone-of-games

--- assets/index--gTGO5F1.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as t,j as s}from"./index-jtVq4ceI.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-kDlPV7qP.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-kDlPV7qP.js","assets/index-jtVq4ceI.js","assets/index-Tt60U99H.css","assets/twitchApi-xET8mJQq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- assets/index-myVaVW-d.js
+++
@@ -1,7 +0,0 @@
-import{r as e,_ as i,a as c,j as t}from"./index-jtVq4ceI.js";const l=e.lazy(()=>i(()=>import("./RawgTopRatedGames-QNWDrwI3.js"),__vite__mapDeps([0,1,2,3,4]))),n=()=>{const[a,r]=e.useState([]);e.useEffect(()=>{o()},[]);const o=async()=>{try{const s=await c.getGamesList;r(s.data.results)}catch(s){console.error("Error fetching top rated games:",s)}};return t.jsx("div",{className:"grid grid-cols-4",children:t.jsx("div",{className:"col-span-4 bg-primary text-text",children:t.jsx(l,{gamesList:a})})})};export{n as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/RawgTopRatedGames-QNWDrwI3.js","assets/index-jtVq4ceI.js","assets/index-Tt60U99H.css","assets/index-h2sFmp9o.js","assets/index-5DV77xRg.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
