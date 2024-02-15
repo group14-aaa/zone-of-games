a03b4b630630abc3e315b223825030ac7c3ada2c
pmAdriaan <pmadriaan@gmail.com>
2024-02-15 09:13:49 +0000

    Build: (eb08b25) fix: Resolve asynchronous issue in Twitch stream fetching

--- assets/index-hxRGvWuC.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as t,j as s}from"./index-LPEFWwsS.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-J1gszxMX.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-J1gszxMX.js","assets/index-LPEFWwsS.js","assets/index-uM071PHf.css","assets/twitchApi-iEN5DqUS.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
