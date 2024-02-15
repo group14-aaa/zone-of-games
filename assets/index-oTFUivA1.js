f7ef5b71943cd1d3b9c3492dd8fb9696a0b0c454
pmAdriaan <pmadriaan@gmail.com>
2024-02-15 17:01:20 +0000

    Build: (22a6994) FIX/REFACTOR: Massive Code Refactor for Homepage to be able to select games by genre and platform

--- assets/index-oTFUivA1.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as t,j as s}from"./index-lY2C8OBL.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-Zpc_mrzR.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-Zpc_mrzR.js","assets/index-lY2C8OBL.js","assets/index--_S3E3C_.css","assets/twitchApi-I-VUWjBs.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
