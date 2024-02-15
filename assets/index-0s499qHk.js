f23b069a146819b694225a92a2309fddd2b4fc23
pmAdriaan <pmadriaan@gmail.com>
2024-02-15 12:10:29 +0000

    Build: (b8172ec) Changed the footer to full width

---
+++ assets/index-0s499qHk.js
@@ -0,0 +1,7 @@
+import{r as e,_ as i,a as c,j as t}from"./index-lY2C8OBL.js";const l=e.lazy(()=>i(()=>import("./RawgTopRatedGames-qm4LBvcI.js"),__vite__mapDeps([0,1,2,3,4]))),n=()=>{const[a,r]=e.useState([]);e.useEffect(()=>{o()},[]);const o=async()=>{try{const s=await c.getGamesList;r(s.data.results)}catch(s){console.error("Error fetching top rated games:",s)}};return t.jsx("div",{className:"grid grid-cols-4",children:t.jsx("div",{className:"col-span-4 bg-primary text-text",children:a?.length>0&&t.jsx(l,{gamesList:a})})})};export{n as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/RawgTopRatedGames-qm4LBvcI.js","assets/index-lY2C8OBL.js","assets/index--_S3E3C_.css","assets/index-APot0Kq9.js","assets/index-TSwrYPXL.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- assets/index-MeVk17V5.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as t,j as s}from"./index-PBvQp5YC.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-ra64c_Je.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-ra64c_Je.js","assets/index-PBvQp5YC.js","assets/index-g-Dqd4Ms.css","assets/twitchApi--5iwyX1r.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
