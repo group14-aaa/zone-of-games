7a9e0a62a2e3c665442c1b0c4f7bf9c9435d8d4b
pmAdriaan <pmadriaan@gmail.com>
2024-02-15 17:10:59 +0000

    Build: (d29e091) FIX/REFACTOR: Massive Code Refactor for Homepage to be able to select games by genre and platform

--- assets/index-1hqV4Fnf.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as t,j as s}from"./index-4LIR1eHY.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-SNqtfCZ2.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-SNqtfCZ2.js","assets/index-4LIR1eHY.js","assets/index--_S3E3C_.css","assets/twitchApi-fmnFiBoP.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
---
+++ assets/index-8amgs0rC.js
@@ -0,0 +1,7 @@
+import{r,_ as t,j as s}from"./index-8FuDbVJe.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-Xb9YuXwD.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-Xb9YuXwD.js","assets/index-8FuDbVJe.js","assets/index--_S3E3C_.css","assets/twitchApi-b0F3QXQj.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
