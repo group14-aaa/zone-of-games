7e0fd2a112e00472f3588c43f31509b24bc108d2
pmAdriaan <pmadriaan@gmail.com>
2024-02-16 17:56:11 +0000

    Build: (7aa9dd4) Add nav link to other pages and files rename

--- assets/index-2-sIzxy4.js
+++
@@ -1,7 +0,0 @@
-import{r as e,_ as i,d as c,j as t}from"./index-MK_xJvdS.js";const l=e.lazy(()=>i(()=>import("./RawgTopRatedGames-IYGDYs9G.js"),__vite__mapDeps([0,1,2,3,4]))),n=()=>{const[a,r]=e.useState([]);e.useEffect(()=>{o()},[]);const o=async()=>{try{const s=await c.getGamesList;r(s.data.results)}catch(s){console.error("Error fetching top rated games:",s)}};return t.jsx("div",{className:"grid grid-cols-4",children:t.jsx("div",{className:"col-span-4 bg-primary text-text",children:a?.length>0&&t.jsx(l,{gamesList:a})})})};export{n as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/RawgTopRatedGames-IYGDYs9G.js","assets/index-MK_xJvdS.js","assets/index--_S3E3C_.css","assets/index-ILnIjBQU.js","assets/index-jHR28Ljs.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- assets/index-IOT8k5VK.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as t,j as s}from"./index-MK_xJvdS.js";const a=r.lazy(()=>t(()=>import("./TwitchTopGames-iTEgFlBx.js"),__vite__mapDeps([0,1,2,3]))),i=()=>s.jsx("div",{className:"grid grid-cols-4",children:s.jsx("div",{className:"col-span-4 bg-primary text-text",children:s.jsx(a,{})})});export{i as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-iTEgFlBx.js","assets/index-MK_xJvdS.js","assets/index--_S3E3C_.css","assets/twitchApi-482K6jI3.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
