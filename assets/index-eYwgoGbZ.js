ec0be61778a2d7a383c8cd3dce2eb25845bd4800
pmAdriaan <pmadriaan@gmail.com>
2024-02-17 13:59:24 +0000

    Build: (9f3d112) Moved Sidebar links to a separate component and changed single to double quotes

--- assets/index-eYwgoGbZ.js
+++
@@ -1,7 +0,0 @@
-import{r as t,_ as a,j as e,L as s}from"./index-pUFErbTU.js";const r=t.lazy(()=>a(()=>import("./TwitchTopGames-KMbXrIpb.js"),__vite__mapDeps([0,1,2,3]))),c=()=>e.jsxs("div",{className:"grid grid-cols-4",children:[e.jsxs("div",{className:"bg-secondary text-text h-full hidden md:block",children:[e.jsx(s,{to:"/streams/",children:e.jsx("div",{className:"p-5 hover:bg-accent hover:text-white ",children:e.jsx("h2",{className:"text-2xl font-bold",children:"Top Games Streaming"})})}),e.jsx(s,{to:"/streams/most-viewed",children:e.jsx("div",{className:"p-5 hover:bg-accent hover:text-white ",children:e.jsx("h2",{className:"text-2xl font-bold",children:"Top 100 Streams"})})}),e.jsx(s,{to:"/games/top",children:e.jsx("div",{className:"p-5 hover:bg-accent hover:text-white",children:e.jsx("h2",{className:"text-2xl font-bold ",children:"Top Rated Games"})})})]}),e.jsx("div",{className:"col-span-4 md:col-span-3 bg-primary text-text",children:e.jsx(r,{})})]});export{c as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TwitchTopGames-KMbXrIpb.js","assets/index-pUFErbTU.js","assets/index-p4idUA-V.css","assets/twitchApi-yrcbYASd.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
